1740b8e99305c0cbbd967cbc0eb173a00aad9dfe
Aryan Arora <aryanarora.w1@gmail.com>
2022-07-26 22:27:44 +0530

    Welcome Text

--- nin.c
+++ nin.c
@@ -5,10 +5,11 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <sys/ioctl.h>
-#include<string.h>
+#include <string.h>
 
 /*** define ***/
 #define CTRL_KEY(k) ((k)&0x1f)
+#define KILO_VERSION "0.0.1"
 
 /*** data ***/
 struct editorConfig
@@ -165,31 +166,48 @@ void editorDrawRows(struct abuf *ab)
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
 
-        
-        abAppend(ab, "~", 1);
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
 
-        abAppend(ab,"\x1b[K",3);//clear line
+        abAppend(ab, "\x1b[K", 3); // clear line
         if (y < E.screenrows - 1)
         {
-            abAppend(ab, "\r\n", 2);//carriage return and next line
+            abAppend(ab, "\r\n", 2); // carriage return and next line
         }
     }
 }
 void editorRefreshScreen()
 {
-    struct abuf ab=ABUF_INIT;
-    abAppend(&ab, "\x1b[?25l", 6);//hide cursor
-    
-    abAppend(&ab, "\x1b[H", 3);  // cursor to top left
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[?25l", 6); // hide cursor
+
+    abAppend(&ab, "\x1b[H", 3); // cursor to top left
 
     editorDrawRows(&ab);
     abAppend(&ab, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[?25h", 6);//show cursor
+    abAppend(&ab, "\x1b[?25h", 6); // show cursor
 
-    write(STDOUT_FILENO,ab.buf,ab.len);
+    write(STDOUT_FILENO, ab.buf, ab.len);
     abFree(&ab);
-
 }
 
 /*** init ***/
