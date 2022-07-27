4ba203f0fe2a3b11481dd2e12a94a3685dc01505
Aryan Arora <aryanarora.w1@gmail.com>
2022-07-27 14:15:05 +0530

    added navigational bounds & pageup pagedown functionality

--- nin.c
+++ nin.c
@@ -16,7 +16,9 @@ enum editorKey
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -95,16 +97,34 @@ int editorReadKey()
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] > '0' && seq[1] <= '9')
             {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                }
             }
         }
         return '\x1b';
@@ -189,16 +209,20 @@ void editorMoveCursor(int key)
     switch (key)
     {
     case ARROW_LEFT:
-        E.cx--;
+        if (E.cx != 0)
+            E.cx--;
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (E.cx != E.screencols - 1)
+            E.cx++;
         break;
     case ARROW_UP:
-        E.cy--;
+        if (E.cy != 0)
+            E.cy--;
         break;
     case ARROW_DOWN:
-        E.cy++;
+        if (E.cy != E.screenrows - 1)
+            E.cy++;
         break;
     }
 }
@@ -214,6 +238,16 @@ void editorProcessKeypress()
 
         exit(0);
         break;
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
+    }
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
@@ -233,7 +267,7 @@ void editorDrawRows(struct abuf *ab)
         if (y == E.screenrows / 3)
         {
             char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+            int welcomelen = snprintf(welcome, sizeof(welcome), "nin editor -- version %s", KILO_VERSION);
             if (welcomelen > E.screencols)
                 welcomelen = E.screencols;
 
