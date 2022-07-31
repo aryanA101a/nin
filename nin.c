7128522f273a84286b5bc957b2f8bea1e53a4307
Aryan Arora <aryanarora.w1@gmail.com>
2022-07-31 21:22:24 +0530

    Improved cursor interaction with tabs

--- nin.c
+++ nin.c
@@ -45,6 +45,7 @@ typedef struct erow
 struct editorConfig
 {
     int cx, cy;
+    int rx;
     int rowoff;
     int coloff;
     int screenrows;
@@ -228,6 +229,19 @@ int getWindowSize(int *rows, int *cols)
     }
 }
 /*** row operations ***/
+int editorRowCxToRX(erow *row, int cx)
+{
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++)
+    {
+        if (row->chars[j] == '\t')
+            rx += (NIN_TAB_STOP - 1) - (rx % NIN_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
     int tabs = 0;
@@ -244,7 +258,7 @@ void editorUpdateRow(erow *row)
     row->render = malloc(row->size + tabs * (NIN_TAB_STOP) + 1);
 
     int idx = 0;
-    //add spaces in places of '/t'
+    // add spaces in places of '/t'
     for (j = 0; j < row->size; j++)
     {
         if (row->chars[j] == '\t')
@@ -412,6 +426,14 @@ void editorProcessKeypress()
 /*** output ***/
 void editorScroll()
 {
+    E.rx = 0;
+    if (E.cy < E.numrows)
+    {
+        E.rx = editorRowCxToRX(&E.row[E.cy], E.cx);
+    }
+
+    
+    // vertical scroll
     if (E.cy < E.rowoff)
     {
         E.rowoff = E.cy;
@@ -420,13 +442,15 @@ void editorScroll()
     {
         E.rowoff = E.cy - E.screenrows + 1;
     }
-    if (E.cx < E.coloff)
+
+    // horizontal scroll
+    if (E.rx < E.coloff)
     {
-        E.coloff = E.cx;
+        E.coloff = E.rx;
     }
-    if (E.cx >= E.coloff + E.screencols)
+    if (E.rx >= E.coloff + E.screencols)
     {
-        E.coloff = E.cx - E.screencols + 1;
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -489,7 +513,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1); // updates the position of cursor/
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6); // show cursor
@@ -504,6 +528,7 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
