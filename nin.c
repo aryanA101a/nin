08ac3b2b9e32a5cbbd1ed90d4e502bf347ea217b
Aryan Arora <aryanarora.w1@gmail.com>
2022-08-07 13:25:01 +0530

    added quit confirmation & dirty flag

--- nin.c
+++ nin.c
@@ -56,6 +56,7 @@ struct editorConfig
     int screencols;
     int numrows;
     erow *row;
+    int dirty;
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -300,6 +301,7 @@ void editorAppendRow(char *s, size_t len)
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
+    E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c)
@@ -311,6 +313,7 @@ void editorRowInsertChar(erow *row, int at, int c)
     row->size++;
     row->chars[at] = c;
     editorUpdateRow(row);
+    E.dirty++;
 }
 
 /*** editor operations ***/
@@ -370,6 +373,7 @@ void editorOpen(char *filename)
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editorSave()
@@ -389,6 +393,7 @@ void editorSave()
             {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editorSetStatusMessage("%d bytes written to disk", len);
                 return;
             }
@@ -473,6 +478,7 @@ void editorMoveCursor(int key)
 }
 void editorProcessKeypress()
 {
+    static int quit_times = 2;
     int c = editorReadKey();
 
     switch (c)
@@ -482,6 +488,14 @@ void editorProcessKeypress()
         break;
 
     case CTRL_KEY('q'):
+        if (E.dirty && quit_times > 0)
+        {
+            editorSetStatusMessage("Warning!!! File has unsaved changes. "
+                                   "Press Ctrl-Q %d more times to quit.",
+                                   quit_times);
+            quit_times--;
+            return;
+        }
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
 
@@ -541,6 +555,7 @@ void editorProcessKeypress()
         editorInsertChar(c);
         break;
     }
+    quit_times = 2;
 }
 
 /*** output ***/
@@ -621,9 +636,9 @@ void editorDrawRows(struct abuf *ab)
 }
 void editorDrawStatusBar(struct abuf *ab)
 {
-    abAppend(ab, "\x1b[7m", 4);
+    abAppend(ab, "\x1b[7m", 4); // invert the colors
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
     if (len > E.screencols)
         len = E.screencols;
@@ -642,7 +657,7 @@ void editorDrawStatusBar(struct abuf *ab)
             len++;
         }
     }
-    abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\x1b[m", 3); // reset inversion of color
     abAppend(ab, "\r\n", 2);
 }
 
@@ -697,6 +712,7 @@ void initEditor()
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
