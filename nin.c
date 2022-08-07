84850c5b2f0fe4dd044c8e42d45e346b0eb7449e
Aryan Arora <aryanarora.w1@gmail.com>
2022-08-07 11:49:38 +0530

    added save file support

--- nin.c
+++ nin.c
@@ -4,6 +4,7 @@
 
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <termios.h>
 #include <time.h>
 #include <stdarg.h>
@@ -62,6 +63,9 @@ struct editorConfig
 };
 
 struct editorConfig E;
+
+/*** prototypes ***/
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 void die(const char *s)
@@ -321,6 +325,29 @@ void editorInsertChar(int c)
 }
 
 /*** file i/o ***/
+char *editorRowsToString(int *buflen)
+{
+    int totlen = 0;
+    int j;
+    for (j = 0; j < E.numrows; j++)
+    {
+        totlen += E.row[j].size + 1;
+    }
+    *buflen = totlen;
+
+    char *buf = malloc(totlen);
+    char *p = buf;
+    for (j = 0; j < E.numrows; j++)
+    {
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+
+    return buf;
+}
+
 void editorOpen(char *filename)
 {
     free(E.filename);
@@ -343,6 +370,33 @@ void editorOpen(char *filename)
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave()
+{
+    if (E.filename == NULL)
+        return;
+
+    int len;
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    if (fd != -1)
+    {
+        if (ftruncate(fd, len) != -1) // sets file size to specified length
+        {
+            if (write(fd, buf, len) == len)
+            {
+                close(fd);
+                free(buf);
+                editorSetStatusMessage("%d bytes written to disk", len);
+                return;
+            }
+        }
+        close(fd);
+    }
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 /*** append buffer ***/
 struct abuf
@@ -432,6 +486,10 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[H", 3);
 
         exit(0);
+        break;
+
+    case CTRL_KEY('s'):
+        editorSave();
         break;
 
     case HOME_KEY:
@@ -656,7 +714,7 @@ int main(int argc, char *argv[])
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-Q = quit");
+    editorSetStatusMessage("HELP:Ctrl+S = save | Ctrl+Q = quit");
 
     while (1)
     {
