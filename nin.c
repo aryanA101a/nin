5fb1651ad839bbe93b5de4d603314a0a9cdd6929
Aryan Arora <aryanarora.w1@gmail.com>
2022-08-13 20:07:55 +0530

    add enter key support

--- nin.c
+++ nin.c
@@ -286,11 +286,11 @@ void editorUpdateRow(erow *row)
     row->render[idx] = '\0';
     row->rsize = idx;
 }
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
-    int at = E.numrows;
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -357,12 +357,25 @@ void editorInsertChar(int c)
 {
     if (E.cy == E.numrows)
     {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows,"", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
 }
-
+void editorInsertNewline(){
+    if(E.cx==0){
+        editorInsertRow(E.cy,"",0);
+    }else{
+        erow *row=&E.row[E.cy];
+        editorInsertRow(E.cy+1,&row->chars[E.cx],row->size-E.cx);
+        row=&E.row[E.cy];
+        row->size=E.cx;
+        row->chars[row->size]='\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx=0;
+}
 void editorDelChar()
 {
     if (E.cy == E.numrows)
@@ -427,7 +440,7 @@ void editorOpen(char *filename)
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             linelen--;
 
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows,line, linelen);
     }
     free(line);
     fclose(fp);
@@ -542,7 +555,7 @@ void editorProcessKeypress()
     switch (c)
     {
     case '\r':
-        // todo
+        editorInsertNewline();
         break;
 
     case CTRL_KEY('q'):
