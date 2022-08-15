90bc2889674723b42d05e7f46b47e10fb0f8888e
Aryan Arora <aryanarora.w1@gmail.com>
2022-08-15 13:02:17 +0530

    add search support

--- nin.c
+++ nin.c
@@ -242,7 +242,7 @@ int getWindowSize(int *rows, int *cols)
     }
 }
 /*** row operations ***/
-int editorRowCxToRX(erow *row, int cx)
+int editorRowCxToRx(erow *row, int cx)
 {
     int rx = 0;
     int j;
@@ -253,6 +253,18 @@ int editorRowCxToRX(erow *row, int cx)
         rx++;
     }
     return rx;
+}
+int editorRowRxToCx(erow* row,int rx){
+    int cur_rx=0;
+    int cx;
+    for(cx=0;cx<row->size;cx++){
+        if(row->chars[cx]=='\t')
+        cur_rx+=(NIN_TAB_STOP-1)-(cur_rx%NIN_TAB_STOP);
+        cur_rx++;
+
+        if(cur_rx>rx)return cx;
+    }
+    return cx;
 }
 
 void editorUpdateRow(erow *row)
@@ -487,7 +499,28 @@ void editorSave()
     free(buf);
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
+/*** find ***/
 
+void editorFind()
+{
+    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    if (query == NULL)
+        return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++)
+    {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match)
+        {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row,match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+}
 /*** append buffer ***/
 struct abuf
 {
@@ -646,6 +679,10 @@ void editorProcessKeypress()
             E.cx = E.row[E.cy].size;
         break;
 
+    case CTRL_KEY('f'):
+    editorFind();
+    break;
+
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -698,7 +735,7 @@ void editorScroll()
     E.rx = 0;
     if (E.cy < E.numrows)
     {
-        E.rx = editorRowCxToRX(&E.row[E.cy], E.cx);
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
     }
 
     // vertical scroll
@@ -864,7 +901,7 @@ int main(int argc, char *argv[])
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP:Ctrl+S = save | Ctrl+Q = quit");
+    editorSetStatusMessage("HELP: Ctrl+S = save | Ctrl+Q = quit | Ctrl+F = find");
 
     while (1)
     {
